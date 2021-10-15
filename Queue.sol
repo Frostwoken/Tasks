b66179bd60aa7ec3abafb44b440158e576dbbbaa
Степан Колмагоров <frostwoken@gmail.com>
2021-10-15 16:54:53 +0700

    Initial commit

--- Queue.sol
+++ Queue.sol
@@ -23,7 +23,7 @@ contract Queue {
     function dequeue() public checkOwnerAndAccept {
         require(queue.length != 0, 105, "Queue is empty.");
         if (queue.length == 1)
-            delete queue;
+            queue.pop();
         else
         {
             for (uint i = 0; i < queue.length - 1; ++i)
