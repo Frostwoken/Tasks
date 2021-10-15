9d5b03e4c7113adf6fd758fd18ecc4ce70932fd4
Степан Колмагоров <frostwoken@gmail.com>
2021-10-15 14:56:54 +0700

    Initial commit

---
+++ Queue.sol
@@ -0,0 +1,34 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract Queue {
+    string[] public queue;
+
+    constructor() public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+    }
+
+    modifier checkOwnerAndAccept {
+	require(msg.pubkey() == tvm.pubkey(), 102);
+	tvm.accept();
+	_;
+    }
+
+    function enqueue(string name) public checkOwnerAndAccept {
+        queue.push(name);
+    }
+
+    function dequeue() public checkOwnerAndAccept {
+        require(queue.length != 0, 105, "Queue is empty");
+        if (queue.length == 1)
+            delete queue;
+        else
+        {
+            for (uint i = 0; i < queue.length - 1; ++i)
+                queue[i] = queue[i + 1];
+            queue.pop();
+        }
+    }
+}
