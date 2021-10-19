580895f75a29d4183233b298a2995d126a54f417
Степан Колмагоров <frostwoken@gmail.com>
2021-10-19 16:49:14 +0700

    Initial commit

--- TaskList.sol
+++ TaskList.sol
@@ -3,13 +3,13 @@ pragma AbiHeader expire;
 
 contract TaskList {
     struct Task {
-        string taskName;
+        string name;
         uint32 timestamp;
-        bool taskCompleted;
+        bool isCompleted;
     }
     mapping (int8 => Task) tasks;
-    int8 index = 0;
-    int8 taskCounter = 0;
+    int8 taskCount = 0;
+    int8 openTaskCount = 0;
 
     constructor() public {
         require(tvm.pubkey() != 0, 101);
@@ -23,20 +23,24 @@ contract TaskList {
 	_;
     }
 
-    function addTask(string taskName) public checkOwnerAndAccept {
-        Task newTask = Task(taskName, now, false);
-        tasks[index] = newTask;
-        index++;
-        taskCounter++;
+    function addTask(string name) public checkOwnerAndAccept {
+        Task newTask = Task(name, now, false);
+        tasks[taskCount] = newTask;
+        taskCount++;
+        openTaskCount++;
     }
 
-    function getNumberOfTasks() public view returns (int8) {
-        return taskCounter;
+    function getNumberOfOpenTasks() public view returns (int8) {
+        return openTaskCount;
     }
     
-    function getListOfTasks() public view returns (mapping (int8 => Task)) {
-        require(taskCounter != 0, 100, "No tasks found.");
-        return tasks;
+    function getListOfTasks() public checkOwnerAndAccept returns (Task[]) {
+        require(openTaskCount != 0, 100, "No tasks found.");
+        Task[] openTasks;
+        for (int8 i = 0; i < taskCount; ++i)
+            if (tasks.exists(i) && tasks[i].isCompleted == false)
+                openTasks.push(tasks[i]);
+        return openTasks;
     }
 
     function getTaskDescriptionByKey(int8 key) public view returns (Task) {
@@ -46,12 +50,14 @@ contract TaskList {
 
     function deleteTaskByKey(int8 key) public checkOwnerAndAccept {
         require(tasks.exists(key), 100, "Key not found.");
+        if (tasks[key].isCompleted == false)
+            openTaskCount--;
         delete tasks[key];
-        taskCounter--;
     }
 
     function completeTask(int8 key) public checkOwnerAndAccept {
         require(tasks.exists(key), 100, "Key not found.");
-        tasks[key].taskCompleted = true;
+        tasks[key].isCompleted = true;
+        openTaskCount--;
     }
 }
